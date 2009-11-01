aad0df00d7a61020d23947d940b93db05d0bc81e
nunojorgelima@gmail.com <nunojorgelima@gmail.com@a89d9cf4-c663-11de-a380-73d83ba7c083>
2009-11-01 01:14:00 +0000

    JFrameContactos +  Main

--- Contactos/src/contactos/Main.java
+++ Contactos/src/contactos/Main.java
@@ -14,9 +14,13 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        
 
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new JFrameContactos().setVisible(true);
+            }
+        });
     }
 
 }

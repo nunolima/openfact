e689fa22ab4628920eeec7620d641ef9884c3a3a
nunojorgelima@gmail.com <nunojorgelima@gmail.com@a89d9cf4-c663-11de-a380-73d83ba7c083>
2009-11-01 00:56:34 +0000

    + uma alteração ao main

--- trunk/Contactos/src/contactos/Main.java
+++ trunk/Contactos/src/contactos/Main.java
@@ -16,6 +16,8 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("Olá mundo!");
+        System.out.println("Mais uma alteração ... só para testar o SVN");
+
     }
 
 }

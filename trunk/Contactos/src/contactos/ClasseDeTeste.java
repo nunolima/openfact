0377ad2dfb3977393699101b73cc62fd020a17bc
nunojorgelima@gmail.com <nunojorgelima@gmail.com@a89d9cf4-c663-11de-a380-73d83ba7c083>
2009-11-01 01:01:41 +0000

    Classe de teste

---
+++ trunk/Contactos/src/contactos/ClasseDeTeste.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package contactos;
+
+/**
+ *
+ * @author User
+ */
+public class ClasseDeTeste {
+
+    private String teste;
+
+    public ClasseDeTeste() {
+    }
+
+    public String getTeste() {
+        return teste;
+    }
+
+    public void setTeste(String teste) {
+        this.teste = teste;
+    }
+
+    
+}

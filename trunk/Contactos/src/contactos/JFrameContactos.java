6753a73b103338dd7ff498c885effa22950c78bb
nunojorgelima@gmail.com <nunojorgelima@gmail.com@a89d9cf4-c663-11de-a380-73d83ba7c083>
2009-11-01 01:14:00 +0000

    JFrameContactos +  Main

--- trunk/Contactos/src/contactos/JFrameContactos.java
+++ trunk/Contactos/src/contactos/JFrameContactos.java
@@ -75,16 +75,6 @@ public class JFrameContactos extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-    * @param args the command line arguments
-    */
-    public static void main(String args[]) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new JFrameContactos().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;

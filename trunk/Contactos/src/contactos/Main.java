5ee8aa7d4254b444b54b4191dc75f7b25885c69e
nunojorgelima@gmail.com <nunojorgelima@gmail.com@a89d9cf4-c663-11de-a380-73d83ba7c083>
2009-11-01 01:12:58 +0000

    Classe de teste

--- trunk/Contactos/src/contactos/Main.java
+++ trunk/Contactos/src/contactos/Main.java
@@ -15,8 +15,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Olá mundo!");
-        System.out.println("Mais uma alteração ... só para testar o SVN");
+        
 
     }
 

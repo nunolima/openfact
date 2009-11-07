46de5db69d2b45dbac1678c669688803e5a4fc38
nunojorgelima@gmail.com <nunojorgelima@gmail.com@a89d9cf4-c663-11de-a380-73d83ba7c083>
2009-11-07 02:27:40 +0000

    ficheiro sql

---
+++ Contactos/src/config/dbase.sql
@@ -0,0 +1,26 @@
+create table pessoas (
+id bigint primary key generated always as identity,
+nome varchar( 32672 ) not null,
+datanasc date,
+morada varchar( 32672 ),
+localidade varchar( 100 ),
+telfcasa varchar( 100 ),
+telfempresa varchar( 100 ),
+telfmovel1 varchar( 100 ),
+telfmovel2 varchar( 100 ),
+telfmovel3 varchar( 100 ),
+activo char(1) default 'A',
+datacriacao timestamp default current timestamp -- current date -- se fosse date
+);
+
+create table categorias (
+id int primary key generated always as identity,
+descricao varchar(100) not null,
+activo integer default 1
+);
+
+create table contactos (
+id_pessoa bigint not null,
+id_categoria int not null,
+primary key (id_pessoa, id_categoria)
+);

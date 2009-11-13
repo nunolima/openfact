42fb0d52746c7f10f645e6af400d014fa00028bb
nunojorgelima@gmail.com <nunojorgelima@gmail.com@a89d9cf4-c663-11de-a380-73d83ba7c083>
2009-11-13 04:45:45 +0000

    funciona ;)

--- trunk/JPA/src/entidades/Livros.java
+++ trunk/JPA/src/entidades/Livros.java
@@ -20,7 +20,6 @@ import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;

eb0573299bdfd2048980bbaacc49b5290c3138d1
nunojorgelima@gmail.com <nunojorgelima@gmail.com@a89d9cf4-c663-11de-a380-73d83ba7c083>
2009-11-14 03:54:05 +0000

    main

--- openfact/src/entidades/Entidades.java
+++ openfact/src/entidades/Entidades.java
@@ -13,6 +13,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
 
 
 /**
@@ -20,6 +21,7 @@ import javax.persistence.ManyToOne;
  * @author User
  */
 @Entity
+@NamedQuery(name="findAllEntidades", query="select e from Entidades e")
 public class Entidades implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
